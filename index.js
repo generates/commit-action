c022064f8a4d9a05fac5025bc724c63d912a14c5
Ian Walter <pub@ianwalter.dev>
2020-10-17 16:58:29 -0400

    Updates

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const logger = createLogger()
 
 async function run () {
   // Commit to the branch specified in inputs.
-  let branch = process.env.INPUT_COMMIT_BRANCH
+  let branch = process.env.INPUT_BRANCH
 
   // If no branch was specified, try to set it to the PR branch.
   if (!branch) branch = dot.get(github.context, 'payload.pull_request.head.ref')
@@ -19,18 +19,18 @@ async function run () {
   // TODO: check if branch exists and create it if necessary.
 
   // Add specified files or all changes.
-  const files = process.env.INPUT_COMMIT_FILES
-    ? process.env.INPUT_COMMIT_FILES.split('\n')
+  const files = process.env.INPUT_FILES
+    ? process.env.INPUT_FILES.split('\n')
     : '.'
   await execa('git', ['add', files])
 
   // Commit the changes.
-  const message = process.env.INPUT_COMMIT_MESSAGE || 'Automated commit'
+  const message = process.env.INPUT_MESSAGE || 'Automated commit'
   await execa('git', ['commit', '-m', message])
 
   // Push the changes back to the branch.
-  const actor = process.env.INPUT_GITHUB_ACTOR || process.env.GITHUB_ACTOR
-  const token = process.env.INPUT_GITHUB_TOKEN
+  const actor = process.env.INPUT_ACTOR || process.env.GITHUB_ACTOR
+  const token = process.env.INPUT_TOKEN
   const repo = process.env.GITHUB_REPOSITORY
   const origin = token
     ? `https://${actor}:${token}@github.com/${repo}.git`
